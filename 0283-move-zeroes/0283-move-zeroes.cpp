f4844cbe7ccfe187ce05c1092264f0c2588df0f6
Nilesh-Yadav11 <ny172005@gmail.com>
2026-07-04 23:28:19 +0530

    Time: 49 ms (5.79%), Space: 24 MB (8.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,6 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         
         for(int i=0;i<nums.size();i++){
+            // swap only when element at which u are is zero
            if(nums[i]==0){
              for(int j=i+1;j<nums.size();j++){
                 if(nums[j]!=0){
