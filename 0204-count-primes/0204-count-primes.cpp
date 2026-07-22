894a4f2e7e88f4c678423018f0273686caedf5b2
Nilesh-Yadav11 <ny172005@gmail.com>
2026-07-22 23:30:38 +0530

    Time: 168 ms (62.78%), Space: 12.6 MB (65.03%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -17,13 +17,11 @@ public:
             }
         }
         int count=0;
-
         for(int i=2;i<n;i++){
             if(isPrime[i]){
                 count++;
             }
         }
-
         return count;
     }
 };
