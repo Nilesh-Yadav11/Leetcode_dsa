c6a1b4cc47aac65deb2e68f0792f16ec8d83b82c
Nilesh-Yadav11 <ny172005@gmail.com>
2026-08-05 12:16:34 +0530

    Time: 0 ms (100%), Space: 14.6 MB (19.32%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        // Brute
+        // int n=nums.size();
+        // vector<int>ans(nums.size());
+        // for(int i=0;i<nums.size();i++){
+        //     int count=0;
+        //     for(int j=0;j<nums.size();j++){
+        //         if(nums[i]>nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     ans[i]=count;
+        // }
+        // return ans;
+
+        // Optimal
+
+        // [8,1,2,2,3]
+
+        // sorting -> 1 2 2 3 8 temp vector 
+        // now map for storing first occurence only ; like 1=0;2=1;3=3;8=4
+        // see that ans is their first occurence only 
+
+        vector<int>temp=nums;
+
+        sort(temp.begin(),temp.end());
+        unordered_map<int,int>mpp;
+        
+        for(int i=0;i<nums.size();i++){
+            if(!mpp.count(temp[i])){
+                mpp[temp[i]]=i;
+            }
+        }
+        vector<int>ans;
+        for(int x:nums){
+            ans.push_back(mpp[x]);
+        }
+
+        return ans;
+    }
+};
